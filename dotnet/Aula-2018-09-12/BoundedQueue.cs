59f4b25b2d3911256d5a547526e7f907cb668c23
jmartins3 <jmartins@isel.pt>
2018-09-14 15:24:03 +0100

    Sources for lecture at 2018/09/12

---
+++ dotnet/Aula-2018-09-12/BoundedQueue.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Aula_2018_09_12
+{
+    /// <summary>
+    /// A try to produce a generic BoundedQueue
+    /// using mutex and event has data and flux control synchronizers, respectively
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BoundedQueue<T>
+    {
+        private LinkedList<T> elems;
+        private int size; // current list size
+
+        private int capacity; // maximum elements on list
+
+        private Mutex mutex;
+
+        private AutoResetEvent spaceAvailable; // signaled when there are 
+        private AutoResetEvent elemsAvailable;
+
+        public BoundedQueue(int capacity)
+        {
+            this.capacity = capacity;
+            this.size = 0;
+            this.elems = new LinkedList<T>();
+
+            this.mutex = new Mutex();
+            this.spaceAvailable = new AutoResetEvent(true);
+            this.elemsAvailable = new AutoResetEvent(false);
+        }
+
+        private bool  IsEmpty()
+        {
+            return size == 0;
+        }
+
+        private bool IsFull()
+        {
+            return size == capacity;
+        }
+        public T Get()
+        {
+            mutex.WaitOne(); // acquire mutex
+
+            if (IsEmpty())
+            { // must wait
+                mutex.ReleaseMutex();
+                elemsAvailable.WaitOne();
+                mutex.WaitOne();
+            }
+            T first = elems.First();
+            elems.RemoveFirst();
+            size--;
+
+            mutex.ReleaseMutex(); // release mutex
+
+            spaceAvailable.Set(); //signal available space
+            return first;
+
+        }
+
+        public void Put(T t)
+        {
+            // to implement
+        }
+    }
+}
---
+++ dotnet/Aula-2018-09-12/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Aula_2018_09_12
+{
+    class Program
+    {
+        /// <summary>
+        /// Estimate context switch time between two threads
+        /// In order to enable counting 1000000 context switch in
+        /// each the two test threads are forced.
+        /// Both threads have maximum priority and the same processor affinity
+        /// in order to force context switches between them.
+        /// </summary>
+        private static void EstimateCtxSwitchTime()
+        {
+            int NSWITCH = 1000000;
+
+             Process.GetCurrentProcess().ProcessorAffinity =
+                new IntPtr(1);
+            ThreadStart func = () =>
+            {
+                for (int i = 0; i < NSWITCH; ++i)
+                    Thread.Yield();
+                
+            };
+
+            Thread t1 = new Thread(func);
+            Thread t2 = new Thread(func);
+
+            t1.Priority = ThreadPriority.Highest;
+            t2.Priority = ThreadPriority.Highest;
+
+            Stopwatch cron = Stopwatch.StartNew();
+            t1.Start();
+            t2.Start();
+
+            // wait for both threads termination
+            t1.Join();
+            t2.Join();
+
+            cron.Stop();
+            long elapsedMillis = cron.ElapsedMilliseconds;
+            Console.WriteLine("Estimated ctx switch time = {0} nanos",
+                (elapsedMillis * 1000000) / (2 * NSWITCH));
+
+        }
+
+        /// <summary>
+        /// Show thread creation
+        /// </summary>
+        private static void Intro()
+        {
+            Console.WriteLine("Hello from main thread {0}",
+                  Thread.CurrentThread.ManagedThreadId);
+            Thread t1 = new Thread(() =>
+            {
+                Thread.Sleep(5000);
+
+                Console.WriteLine("Hello from thread {0} ",
+                    Thread.CurrentThread.ManagedThreadId);
+            });
+            //Uncomment next line and explain the different behaviour
+            //t1.IsBackground = true;
+            t1.Start();
+        }
+
+
+        static void Main(string[] args)
+        {
+            //Intro
+            EstimateCtxSwitchTime();
+        }
+    }
+}
