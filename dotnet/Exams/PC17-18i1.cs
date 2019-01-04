b061fc6c711de358cff947b7e2e4e44da34f7b75
jmartins3 <jmartins@isel.pt>
2019-01-04 11:37:50 +0000

    Preparing exercises serie 3

--- dotnet/Exams/PC17-18i1.cs
+++ dotnet/Exams/PC17-18i1.cs
@@ -6,6 +6,54 @@ using System.Threading.Tasks;
 
 namespace Testes {
     class PC17_18i1 {
+
+        // MapReduce
+
+        public R Map<T, R>(T t) { return default(R); }
+        public R Reduce<R>(R r1, R r2) { return default(R); }
+
+        /// <summary>
+        /// Versão sequencial(síncrona)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="elems"></param>
+        /// <param name="initial"></param>
+        /// <returns></returns>
+        public R MapReduce<T, R>(T[] elems, R initial) {
+            for (int i = 0; i < elems.Length; ++i)
+                initial = Reduce(Map<T, R>(elems[i]), initial);
+            return initial;
+        }
+
+        /// Variantes assíncronas de Map e Reduce
+        /// 
+        public Task<R> MapAsync<T, R>(T t) { return null; }
+        public Task<R> ReduceAsync<R>(R r1, R r2) { return null; }
+
+        /// <summary>
+        /// Versão assíncrona sem preocupação em tratar eventuais execepções
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="elems"></param>
+        /// <param name="initial"></param>
+        /// <returns></returns>
+        public async Task<R> MapReduceAsync<T, R>(T[] elems, R initial) {
+            Task<R>[] tres = new Task<R>[elems.Length];
+
+            for (int i = 0; i < elems.Length; ++i)
+                tres[i] = MapAsync<T, R>(elems[i]);
+
+            // reduce by order
+            for (int i = 0; i < elems.Length; ++i)
+                initial = await ReduceAsync(await tres[i], initial);
+            
+            return initial;
+        }
+
+
+        // GetTopN
         private class BoundedOrderedQueue<T> {
             public BoundedOrderedQueue(int capacity) {
 
